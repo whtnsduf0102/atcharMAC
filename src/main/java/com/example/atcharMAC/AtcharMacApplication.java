8377ecaa17b3d762c332c9835cb7fc21873fe025
조순열 <chosunyeol@josun-yeol-ui-MacBookPro.local>
2022-11-09 21:38:28 +0900

    Initial commit

---
+++ src/main/java/com/example/atcharMAC/AtcharMacApplication.java
@@ -0,0 +1,13 @@
+package com.example.atcharMAC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtcharMacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtcharMacApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/atcharMAC/AtcharMacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.atcharMAC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtcharMacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
